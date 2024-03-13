e906ef43c48f164d94e7d45eb9831fbe644d47b7
Jacob Fielding <jf0517@gmail.com>
2024-03-12 21:43:59 -0700

    feat: swift package setup and OSRM models

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# osrm-openapi
-OpenAPI spec for generating OSRM Models
+# OSRM OpenAPI
+
+OpenAPI spec and generation script to build OSRM Models for various platforms.
+
+## Building OSRM Models
+
+```sh
+./generate_models.sh swift
+```
+
+* `--clean` delete the .build and generated folder.
+
+## References 
+
+- OSRM Open API Initial Source - <https://github.com/1papaya/osrm-openapi>
---
+++ generate_models.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env zsh
+
+set -e
+set -u
+
+clean=false
+
+language="$1"
+shift
+
+for arg in "$@"
+do
+     case $arg in
+          --clean)
+               clean=true
+               shift # Remove --release from processing
+               ;;
+          *)
+               shift # Ignore other argument from processing
+               ;;
+     esac
+done
+
+# Clean the .build directory
+if $clean; then
+     if [ -d ".build" ]; then
+          rm -r .build
+     fi
+     mkdir -p .build
+fi
+
+if [ "$language" = "swift" ]; then
+     # Clean the Generated Swift Models
+     if $clean; then
+          if [ -d "./Sources/OSRM/Models" ]; then
+               rm -r ./Sources/OSRM/Models
+          fi
+          mkdir -p ./Sources/OSRM/Models
+     fi
+
+     openapi-generator generate -i ./openapi.yaml -g swift5 --strict-spec=true \
+          -o .build --global-property models --model-package Models \
+          --skip-validate-spec --additional-properties=useJsonEncodable=false
+
+     swiftformat .build/OpenAPIClient/Classes/OpenAPIsModels
+
+     cp .build/OpenAPIClient/Classes/OpenAPIsModels/* ./Sources/OSRM/Models
+else
+     echo "Language not supported"
+     exit 1
+fi
